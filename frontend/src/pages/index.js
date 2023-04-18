f114ebd31ceae0efe56fc74a5c03f13676e881dd
lachiem1 <72180097+lachiem1@users.noreply.github.com>
2023-04-18 20:52:28 +1000

    add comments for frontend

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -9,24 +9,30 @@ const IndexPage = () => {
     getData();
   }, []);
 
+  // get data from api upon page load/reload
   let getData = async () => {
     let response = await fetch("http://localhost:8000/api/clicked/1/");
     let data = await response.json();
+    // when data is returned then setClickInfo and setLoading to be false so click info can be displayed
     setClickInfo(data);
     setLoading(false);
-    console.log("getData ->data: ", data);
-    console.log("getData ->clickInfo: ", clickInfo);
+    // console.log("getData ->data: ", data);
+    // console.log("getData ->clickInfo: ", clickInfo);
   };
 
+  // runs onClick for increase count button  
   const updateData = async () => {
+    // increment counter by 1 by getting clickInfo object and modifying num_clicks property
     const newInfo = {
       ...clickInfo,
       num_clicks: clickInfo.num_clicks + 1
     }
 
+    // overwrite clickInfo and set it
     clickInfo = newInfo;
     setClickInfo(newInfo);
 
+    // this line is hardcoded...
     await fetch("http://localhost:8000/api/clicked/1/", {
       method: 'PUT',
       headers: {
@@ -34,9 +40,10 @@ const IndexPage = () => {
       },
       body: JSON.stringify({ ...clickInfo, 'updated': new Date() })
     });
-    console.log("update -> clickInfo: ", clickInfo.num_clicks)
+    // console.log("update -> clickInfo: ", clickInfo.num_clicks)
   };
 
+  // runs onClick for reset count button
   const resetData = async () => {
     const newInfo = {
       ...clickInfo,
@@ -59,7 +66,7 @@ const IndexPage = () => {
 
   return (
     <div>
-
+      {/* Will be updated each time clickInfo is set */}
       {loading ? <div>...</div> : <div>This button has been mashed {clickInfo.num_clicks} times.</div>}
       
       <button onClick={() => {
